075008ed2cc4ad8c06bdc45547d89fff829dcba2
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-11-23 05:03:04 +0800

    added key?

--- .github/actions/deploy-to-s3-js/main.js
+++ .github/actions/deploy-to-s3-js/main.js
@@ -3,5 +3,7 @@ const github = require("@actions/github")
 const exec = require("@actions/exec")
 function run() {
   core.notice("Deploying to S3")
+  const apikey = core.getInput("api-key")
+  core.notice("the apikey is " + apikey)
 }
 run()
